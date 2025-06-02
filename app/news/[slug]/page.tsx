eb5117b62c341dd3ae6ab2814a5fe0b1506edd11
Apple <ltrntxs@gmail.com>
2025-06-02 12:38:21 +0900

    Basic Auth🗝️

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,28 +1,41 @@
-import { notFound } from "next/navigation";
-import { getNewsDetail } from "@/app/_libs/microcms";
-import Article from "@/app/_components/Article";
-import ButtonLink from "@/app/_components/ButtonLink";
-import styles from "./page.module.css";
-
-export async function generateStaticParams() {
-  return [
-    { slug: "sample-1" },
-    { slug: "sample-2" },
-  ];
-}
+import { notFound } from 'next/navigation';
+import type { Metadata } from 'next';
+import { getNewsDetail } from '@/app/_libs/microcms';
+import Article from '@/app/_components/Article';
+import ButtonLink from '@/app/_components/ButtonLink';
+import styles from './page.module.css';
 
 type Props = {
-  params: { slug: string };
-  searchParams?: { dk?: string };
+  params: {
+    slug: string;
+  };
+  searchParams: {
+    dk?: string;
+  };
 };
 
-export const revalidate = 0;
-
-export default async function Page(props: Props) {
-  const { params, searchParams } = props;
-
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
   const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams?.dk,
+    draftKey: searchParams.dk,
+  });
+
+  return {
+    title: data.title,
+    description: data.description,
+    openGraph: {
+      title: data.title,
+      description: data.description,
+      images: [data?.thumbnail?.url ?? ''],
+    },
+  };
+}
+
+export default async function Page({ params, searchParams }: Props) {
+  const data = await getNewsDetail(params.slug, {
+    draftKey: searchParams.dk,
   }).catch(notFound);
 
   return (
