b55af8ceedcbe38fc079b19eafcdc5b5db361855
Apple <ltrntxs@gmail.com>
2025-06-03 11:10:27 +0900

    ContactForm🌜

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -23,7 +23,7 @@ export default function Menu() {
                     <Link href="/members">メンバー</Link>
                 </li>
                 <li>
-                    <Link href="/news">お問い合わせ</Link>
+                    <Link href="/contact">お問い合わせ</Link>
                 </li>
             </ul>
             <button className={cx(styles.button, styles.close)} onClick={close}>
